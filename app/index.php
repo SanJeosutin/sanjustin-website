bd231f7b44d74fc9bb1e6533ec82c855f3df6865
Justin San <santosojustin@gmail.com>
2021-10-21 02:41:07 +1100

    Create index.php

---
+++ app/index.php
@@ -0,0 +1,18 @@
+<?php
+include("reusable/header.php");
+include("reusable/navbar.php");
+?>
+<!--Main Content START-->
+<p class="lead animate">
+    Welcome to my new website!. You can find <kbd>anything</kbd> all the
+    stuff I did during my uni year in here, well... maybe not <em>all</em> but most stuff I think it's cool
+    / intresting enough that I've been working on. 'nywho enjoy the snoop :D!
+</p>
+<br>
+<p class="lead">
+    <h5>Current Project:</h5>
+</p>
+<!--Main Content END-->
+<?php
+include("reusable/footer.php");
+?>
