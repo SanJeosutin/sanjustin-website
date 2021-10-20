e02e301ae31ac04598c5a5421de2311d4eecd217
Justin San <santosojustin@gmail.com>
2021-10-21 02:41:11 +1100

    Create about.php

---
+++ app/about.php
@@ -0,0 +1,13 @@
+<?php
+include("reusable/header.php");
+include("reusable/navbar.php");
+?>
+<!--Main Content START-->
+<p class="lead animate">
+    <s>10</s>
+    Hi there, I'm Justin the developer of this very website you've decided to visit. Yay! 
+</p>
+<!--Main Content END-->
+<?php
+include("reusable/footer.php");
+?>
