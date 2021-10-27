34e2135ea44e40573c64be43f81c3ef4dc2634f2
Justin San <santosojustin@gmail.com>
2021-10-28 02:49:40 +1100

    Update card.php

--- app/classes/components/card.php
+++ app/classes/components/card.php
@@ -1,12 +1,12 @@
 <?php
     class Card{
-        private static $projectDetails = array(); 
+        private static $details = array(); 
 
         static function create($title, $description, $demoPath, $githubLink){
-            array_push(self::$projectDetails, $title);
-            array_push(self::$projectDetails, $description);
-            array_push(self::$projectDetails, $demoPath);
-            array_push(self::$projectDetails, $githubLink);
+            array_push(self::$details, $title);
+            array_push(self::$details, $description);
+            array_push(self::$details, $demoPath);
+            array_push(self::$details, $githubLink);
         }
         
         /*
@@ -17,18 +17,18 @@
 
         static function display(){
             echo "<div class=\"row\">";
-            for($i = 0; $i < sizeof(self::$projectDetails); $i+=4){
+            for($i = 0; $i < sizeof(self::$details); $i+=4){
                 echo "
                 <!--Card START -->
                 <div class=\"col-sm-4 py-2\">
                     <div class=\"card h-100\" style=\"width: 18rem;\">
-                        <h5 class=\"card-header\">".self::$projectDetails[$i]."</h5>
+                        <h5 class=\"card-header\">".self::$details[$i]."</h5>
                         <div class=\"card-body\">
-                            <p class=\"card-text\">".self::$projectDetails[$i+1]."</p>
+                            <p class=\"card-text\">".self::$details[$i+1]."</p>
                         </div>
                         <div class=\"card-footer\">
-                            <a href=\"http://".self::$projectDetails[$i+2]."\" class=\"btn btn-primary\">Live Demo</a>
-                            <a href=\"http://".self::$projectDetails[$i+3]."\" class=\"btn btn-secondary float-right\"><i class=\"fab fa-github\"></i> Source Code</a>
+                            <a href=\"http://".self::$details[$i+2]."\" class=\"btn btn-primary\">Live Demo</a>
+                            <a href=\"http://".self::$details[$i+3]."\" class=\"btn btn-secondary float-right\"><i class=\"fab fa-github\"></i> Source Code</a>
                         </div>
                     </div>
                 </div>
