9e693a3dcf60ba3c98c686d75369d17686bbd389
Justin San <santosojustin@gmail.com>
2021-12-17 05:27:03 +1100

    Finish Gallery implementation

--- app/gallery.php
+++ app/gallery.php
@@ -12,38 +12,53 @@ $ig = new Instagram();
 
 $result = $ig->getUserPost();
 if (isset($_GET['RenderPage'])) {
-    foreach($result as $res){
-        $caption = substr($res->caption, 0, strpos($res->caption, "#"));
-        $type = substr($res->caption, strpos($res->caption, '#') + 1);
 
-        Image::create($caption, $res->media_url, $type, $res->timestamp, $res->permalink);
-
-        //switch ($_GET['RenderPage']) {
-        switch ($type) {
+    switch ($_GET['RenderPage']) {
+        //switch ($type) {
             case 'photography':
-                Content::renderGalery("In here you can find some photos I took, ranging from new ones to old ones. Enjoy!", "");
-                Image::display();
+                foreach($result as $res){
+                    $caption = substr($res->caption, 0, strpos($res->caption, "#"));
+                    $type = substr($res->caption, strpos($res->caption, '#') + 1);
+                    
+                    if($type == "photography"){
+                        Image::create($caption, $res->media_url, $type, $res->timestamp, $res->permalink);
+                    }
+                }
 
+                    Content::renderGalery("In here you can find some photos I took, ranging from new ones to old ones. Enjoy!", "");
+                    Image::display();
                 break;
 
             case 'coolstuff':
-                Content::renderGalery("In here you can find some cool stuff I did, ranging from new ones to old ones. Enjoy!", "");
-                Image::display();
+                foreach($result as $res){
+                    $caption = substr($res->caption, 0, strpos($res->caption, "#"));
+                    $type = substr($res->caption, strpos($res->caption, '#') + 1);
+                    if($type == "coolstuff"){
+                        Image::create($caption, $res->media_url, $type, $res->timestamp, $res->permalink);
+                    }
+                }
+                    Content::renderGalery("In here you can find some cool stuff I did, ranging from new ones to old ones. Enjoy!", "");
+                    Image::display();
 
                 break;
 
             case 'designs':
-                Content::renderGalery("In here you can find some designs I've done, ranging from new ones to old ones. Enjoy!", "");
-                Image::display();
-
+                foreach($result as $res){
+                    $caption = substr($res->caption, 0, strpos($res->caption, "#"));
+                    $type = substr($res->caption, strpos($res->caption, '#') + 1);
+                    
+                    if($type == "design"){
+                        Image::create($caption, $res->media_url, $type, $res->timestamp, $res->permalink);
+                    }
+                }
+                    Content::renderGalery("In here you can find some designs I've done, ranging from new ones to old ones. Enjoy!", "");
+                    Image::display();
                 break;
 
             default:
                 Content::renderGalery("Ah well well.. you've found a secret ;D. Good on ya.", "");
                 break;
         }
-    }
-
 }
 
 include("reusable/footer.php");
