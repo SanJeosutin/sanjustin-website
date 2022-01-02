71173841e67d2ee888ea752fc4cf311c4f6ed6e5
Justin San <santosojustin@gmail.com>
2022-01-02 15:17:37 +1100

    Update index.php

--- app/index.php
+++ app/index.php
@@ -2,22 +2,9 @@
 include("reusable/header.php");
 include("reusable/navbar.php");
 
-include_once("classes/components/card.php");
-?>
-<!--Main Content START-->
-<div class="@animate">
-    <p class="lead">
-        Hey, hey! So.. uh.. new website, yea.. uhh mainly I'll be putting my projects that
-        I've been working on during my spare time and some other stuff as well that most likely
-        unrelated to programming. So... happy browsing!
-    </p>
-    <br>
-    <h5>Latest Stuff:</h5>
+include("classes/components/renderContent.php");
 
-    
+Content::render("HOME");
 
-</div>
-<!--Main Content END-->
-<?php
 include("reusable/footer.php");
 ?>
