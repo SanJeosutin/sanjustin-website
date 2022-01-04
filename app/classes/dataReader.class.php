412bfebdbd90b84d7c55f54525a0bfd329157540
Justin San <santosojustin@gmail.com>
2022-01-04 23:47:07 +1100

    working on data renderer class

--- app/classes/dataReader.class.php
+++ app/classes/dataReader.class.php
@@ -1,17 +1,33 @@
 <?php
 class DataReader{
-    static private function readRawData($source){
-        $rawData = fopen($source, "r") or die ("Unable to open file!");
-        fread($rawData,filesize($source));
-        fclose($rawData);
-    }
+    static function readRawData($source){
+        $i = 1;
+        $rawdata = array();
+        $handle = fopen($source, "r") or die ("Unable to open file!");
 
-    static function format($symbol){
-        $rawData = self::readRawData("../data/2022 Goal.txt");
-        foreach ($rawData as $data) {
-            if(preg_match("/{$symbol}/", $data)){
-                $data = str_replace("{$symbol}", "", $data);
-                echo $data;
+        if(is_readable($source)){
+            while(!feof($handle)){
+                $line = fgets($handle);
+                if(trim($line) != ""){
+                    array_push($rawdata, $line);
+                    echo "$i<br>";
+                    $prevLine = $rawdata[$i-1];
+                    $currLine = $line;
+                    echo "PREV: ".$prevLine."<br>";
+                    echo "CURR: ".$currLine."<br>";
+                    var_dump($rawdata);
+                    echo "<br><br>";
+                    $i++;
+                }
+                
+
+                //if(substr($prevLine, 1) == "#TITLE"){
+                //    echo "<p class=\"lead\">".substr($currLine, 1)."</p>";
+                //}
+                //else{
+                //    echo "<p>$currLine</p>";
+                //}
+                
             }
         }
     }
