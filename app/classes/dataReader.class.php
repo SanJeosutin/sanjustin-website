1763379cff6b72de7c3cf26705b6ddf294d99c5f
Justin San <santosojustin@gmail.com>
2022-01-14 01:55:39 +1100

    Refactoring dataRenderer class

--- app/classes/dataReader.class.php
+++ app/classes/dataReader.class.php
@@ -2,7 +2,7 @@
 class DataReader{
     static function readRawData($source){
         $i = 0;
-        $prevLine = "";
+
         $rawdata = array();
         $handle = fopen($source, "r") or die ("Unable to open file!");
 
@@ -10,70 +10,86 @@ class DataReader{
             while(!feof($handle)){
                 $line = fgets($handle);
 
-                if(trim($line) != ""){
+                if (trim($line) != "") {
                     array_push($rawdata, $line);
-                    //echo "$i<br>";
-
-                    if($i != 0){
-                        $prevLine = $rawdata[$i-1];
-                    }
-                    $currLine = $line;
-
-                    if(strpos($prevLine, "#TITLE") !== false){
-                        echo "<p class=\"lead\">$currLine</p>";
-                    }elseif(strpos($prevLine, "#DESCRIPTION") !== false){
-                        echo "<p>$currLine</p>";
-                    }elseif(strpos($prevLine, "#LIST") !== false){
-                        if(strpos($currLine, "//") !== false){
-                            echo "
-                                <table class=\"table\">
-                                    <thead>
-                                        <tr>
-                            ";
-                            $currLine = str_replace("//", "", $currLine);
-                            $wordInLine = explode("|", $currLine);
-
-                            foreach($wordInLine as $word){
-                                echo "
-                                            <th scope=\"col\">$word</th>
-                                ";
-                            }
-                        }
-                        echo "
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                        ";
-                    }
-
-                    if (strpos($currLine, "[") !== false) {
-                        echo"<tr>";
-                        $currLine = str_replace("[", "", $currLine);
-                        $currLine = str_replace("]", "", $currLine);
-                        $wordInLine = explode(",", $currLine);
-
-                        foreach($wordInLine as $word){
-                            echo "
-                                            <td>$word</td>
-                            ";
-                        }
-
-                        echo"
-                                        </tr>
-
-                        ";
-                    }
-
-                    if(strpos($currLine, "#END") !== false){
-                        echo "
-                        </tbody>
-                                    </table>
-                        ";
-                    }
-                    $i++;
                 }
             }
         }
+
+        foreach($rawdata as $data){
+            //echo "<pre>$data</pre>";
+            
+            switch($data){
+                case strpos($data, "#TITLE") !== false:
+                    echo self::setTitle($rawdata[$i+1]);
+                    break;
+                
+                case strpos($data, "#DESCRIPTION") !== false:
+                    echo self::setDescription($rawdata[$i+1]);
+                    break;
+                
+                case strpos($data, "#LIST") !== false:
+                    echo self::setList($rawdata[$i+1]);
+                    break;
+            }
+            
+            $i++;
+        }
+    }
+
+    private static function setTitle($title){
+        return "<p class=\"lead\">$title</p>";
+    }
+
+    private static function setDescription($description){
+        return "<p>$description</p>";
+    }
+
+    private static function setList($lists){
+        $table = "";
+        $headContent = array();
+        $mainContent = array();
+
+        $lists = explode(";", $lists);
+        
+        foreach($lists as $list){
+            if(strpos($list, "|") !== false){
+                $l = explode("|", $list);
+                foreach($l as $header){
+                    array_push($headContent, $header);
+                }
+            }
+
+            if(strpos($list, ",") !== false){
+                $l = explode(",", $list);
+                foreach($l as $main){
+                    array_push($mainContent, $main);
+                }
+            }
+        }
+
+        $table .= "<table class=\"table table-striped\">";
+        $table .= "<thead><tr>";
+        
+        foreach($headContent as $content){
+            $table .= "<th scope=\"col\">$content</th>";
+        }
+
+        $table .= "</tr></thead>";
+        $table .= "<tbody>";
+        
+
+        foreach($mainContent as $content){
+            $table .= "<tr>";
+            for ($i=0; $i < count($headContent); $i++) { 
+                $table .= "<td>$mainContent[$i]</td>";
+            }
+            $table .= "</tr>";
+        }
+
+        $table .= "</tr></tbody>";
+        $table .= "</table>";
+        return $table;
     }
 }
 ?>
