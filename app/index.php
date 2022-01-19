380c0a269cb880789835b10261f06810331e84fe
Justin San <santosojustin@gmail.com>
2022-01-20 00:41:13 +1100

    Update index.php

--- app/index.php
+++ app/index.php
@@ -4,7 +4,17 @@ include("reusable/navbar.php");
 
 include("classes/components/renderContent.php");
 
-Content::render("HOME");
+if (isset($_GET['RenderPage'])) {
+    switch($_GET['RenderPage']){
+        case 'game':
+            include_once("../config/easterEgg/simplePlatformer/");
+
+            Content::render("GAME", array("secret", "Welp, you got me. I'm not sure what this is, but I'm sure it's not a secret anymore."));
+            break;
+    }
+}else{
+    Content::render("HOME");
+}
 
 include("reusable/footer.php");
 ?>
